be539f08135c7cbfe0f8d2498a0e8b1493f9ac94
Matthieu Napoli <matthieu@mnapoli.fr>
2014-09-24 10:52:51 +1200

    Added an option to re-sync repositories

--- src/DashboardBundle/Controller/DashboardController.php
+++ src/DashboardBundle/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace Piwik\DashboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DashboardController extends Controller
 {
@@ -15,5 +16,19 @@ class DashboardController extends Controller
         return $this->render('@Dashboard/Dashboard/dashboard.html.twig', [
             'repositories' => $repositories,
         ]);
+    }
+
+    public function syncAction()
+    {
+        $repositoryProvider = $this->get('dashboard.repository_provider');
+
+        $repositoryProvider->syncRepositories($this->getUser());
+
+        $this->get('session')->getFlashBag()->add(
+            'success',
+            'The repositories have been synchronized with GitHub'
+        );
+
+        return new Response('', 204);
     }
 }
